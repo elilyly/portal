dd464ea08015cf2dc09ff893513326b5e0d96d89
Elizabeth <elizabethnich19@gmail.com>
2017-08-17 16:47:28 -0400

    hover option for doctor info

--- src/UpcomingAppts.js
+++ src/UpcomingAppts.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import Modal, {closeStyle} from 'simple-react-modal'
+import ModalFeature from './ModalFeature'
 
 export default class UpcomingAppts extends Component {
 
   render() {
     return (
-      <div className="ApptContainer">
-          <h2>Upcoming Appointments</h2>
-          <p>Date: 8/28/17  Physician Name: Dr. Collins, MD Time: 10:30 am</p>
-          <p>Date: 8/28/17  Physician Name: Dr. Collins, MD Time: 10:30 am</p>
-          <p>Date: 8/28/17  Physician Name: Dr. Collins, MD Time: 10:30 am</p>
-          <p>Date: 8/28/17  Physician Name: Dr. Collins, MD Time: 10:30 am</p>
+      <div className="apptTable">
+        <h2>Upcoming Appointments</h2>
+        <tr>
+          <th>Appointment</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Physician</th>
+          <th>Location</th>
+        </tr>
+        <tr>
+          <td><a href="http://localhost:3000/appt">Follow-Up</a></td>
+          <td>8/28/17</td>
+          <td>10:30 am</td>
+          <td><ModalFeature /></td>
+          <td>205 E 22nd St, New York, NY 10010</td>
+        </tr>
 
+        <tr>
+          <td>Follow-Up</td>
+          <td>9/17/17</td>
+          <td>10:00 am</td>
+          <td><ModalFeature /></td>
+          <td>205 E 22nd St, New York, NY 10010</td>
+        </tr>
     </div>
+
     )
   }
 }
